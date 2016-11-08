18a62b96d22ee4763d123c5b260b2e09af23dd89
Samuel ROZE <samuel.roze@gmail.com>
2016-11-08 16:55:33 +0000

    Updated with shellcheck

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ set -e
 
 
 if [ $# -ne 1 ]; then
-  echo "Usage: `basename $0` <tag>"
+  echo "Usage: $(basename "$0") <tag>"
   exit 65
 fi
 
@@ -16,20 +16,20 @@ TAG=$1
 # Build master branch
 #
 git checkout master
-git tag ${TAG}
+git tag "${TAG}"
 
 # Add the build details
-cat $FILE_NAME | awk 'NR==2 {print "\# Version: '$TAG'"} 1' > $FILE_NAME.build
+awk 'NR==2 {print "\# Version: '"$TAG"'"} 1' $FILE_NAME > $FILE_NAME.build
 
 #
 # Copy executable file into GH pages
 #
 git checkout gh-pages
 
-cp $FILE_NAME.build downloads/$FILE_NAME-$TAG
-git add downloads/$FILE_NAME-$TAG
+cp $FILE_NAME.build downloads/$FILE_NAME-"$TAG"
+git add downloads/$FILE_NAME-"$TAG"
 
-SHA1=$(openssl sha1 downloads/$FILE_NAME-$TAG | awk '{print $2}')
+SHA1=$(openssl sha1 downloads/$FILE_NAME-"$TAG" | awk '{print $2}')
 
 JSON='name:"'$FILE_NAME'"'
 JSON="${JSON},sha1:\"${SHA1}\""
@@ -39,13 +39,13 @@ JSON="${JSON},version:\"${TAG}\""
 #
 # Update manifest
 #
-cat manifest.json | jsawk -a "this.push({${JSON}})" | python -mjson.tool > manifest.json.tmp
+jsawk -a "this.push({${JSON}})" < manifest.json | python -mjson.tool > manifest.json.tmp
 mv manifest.json.tmp manifest.json
 git add manifest.json
 
 # Symlink latest version
 rm downloads/$FILE_NAME-latest
-cp downloads/$FILE_NAME-${TAG} downloads/$FILE_NAME-latest
+cp downloads/$FILE_NAME-"${TAG}" downloads/$FILE_NAME-latest
 git add downloads/$FILE_NAME-latest
 
 git commit -m "Release version ${TAG}"
