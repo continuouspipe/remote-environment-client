fffb78f8e0f8313d6b14de451380c87e2e4887a6
Alessandro Zucca <alessandro.zucca@gmail.com>
2017-01-06 16:50:09 +0000

    Initial work for the setup command (#62)
    * Starting to port the setup command
    * adding code that creates (or open) the settings file
    * adding function that applies the default value to an optional configuration variable
    * writing setup file on disk using templates
    * Requesting the user the mandatory configuration variables until they are inserted

--- cmd/setup.go
+++ cmd/setup.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 
+	envconfig "github.com/continuouspipe/remote-environment-client/config"
 	"github.com/spf13/cobra"
 )
+
+type reader func(string) string
 
 var setupCmd = &cobra.Command{
 	Use:   "setup",
@@ -14,10 +20,64 @@ var setupCmd = &cobra.Command{
 Your answers will be stored in a .cp-remote-env-settings file in the project root. You 
 will probably want to add this to your .gitignore file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("setup called")
+		execute(cmd, args)
 	},
+}
+
+func execute(cmd *cobra.Command, args []string) {
+	projectKey := repeatIfEmpty(readString, "What is your Continuous Pipe project key?")
+	remoteBranch := repeatIfEmpty(readString, "What is the name of the Git branch you are using for your remote environment?")
+
+	namespace := strings.Replace(remoteBranch, "/", "-", -1)
+	namespace = strings.Replace(namespace, "\\", "-", -1)
+	namespace = projectKey + "-" + namespace
+
+	config := &envconfig.ConfigData{
+		ProjectKey:           projectKey,
+		RemoteBranch:         remoteBranch,
+		RemoteName:           applyDefault(readString, "What is your github remote name? (defaults to: origin)", "origin"),
+		DefaultContainer:     readString("What is the default container for the watch, bash, fetch and resync commands? (Optional)"),
+		ClusterIp:            repeatIfEmpty(readString, "What is the IP of the cluster?"),
+		Username:             repeatIfEmpty(readString, "What is the cluster username?"),
+		Password:             repeatIfEmpty(readString, "What is the cluster password?"),
+		AnybarPort:           readString("If you want to use AnyBar, please provide a port number e.g 1738 ?"),
+		KeenWriteKey:         readString("What is your keen.io write key? (Optional, only needed if you want to record usage stats)"),
+		KeenProjectId:        readString("What is your keen.io project id? (Optional, only needed if you want to record usage stats)"),
+		KeenEventCollection:  readString("What is your keen.io event collection?  (Optional, only needed if you want to record usage stats)"),
+		Namespace:            projectKey + remoteBranch,
+	}
+
+	config.SaveOnDisk()
+	fmt.Printf("\nRemote settings written to %s\n", envconfig.SettingsFileDir())
 }
 
 func init() {
 	RootCmd.AddCommand(setupCmd)
+}
+
+func readString(q string) string {
+	fmt.Print(q, " ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func applyDefault(ask reader, question string, predef string) string {
+	res := ask(question)
+
+	if res == "" && predef != "" {
+		return predef
+	}
+	return res
+}
+
+func repeatIfEmpty(r reader, question string) string {
+	var res string
+	for {
+		res = r(question)
+		if len(res) > 0 {
+			break
+		}
+	}
+	return res
 }
