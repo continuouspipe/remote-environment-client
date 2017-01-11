379bb61dc2daf27b1bb39ef6202a2a6d70b00226
richardmiller <rmiller@inviqa.com>
2017-01-11 19:35:18 +0000

    Add finding a pod by container name

--- kubeapi/pods.go
+++ kubeapi/pods.go
@@ -1,39 +1,54 @@
 package kubeapi
 
 import (
+	"errors"
+	"strings"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func readConfig(context string) (*rest.Config, error) {
+func ReadConfig(context string) (*rest.Config, error) {
 	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		clientcmd.NewDefaultClientConfigLoadingRules(),
 		&clientcmd.ConfigOverrides{CurrentContext: context}).ClientConfig()
 }
 
-func createClient(config *rest.Config) (*kubernetes.Clientset, error) {
+func CreateClient(config *rest.Config) (*kubernetes.Clientset, error) {
 	return kubernetes.NewForConfig(config)
 }
 
 func FetchPods(context string, environment string) (*v1.PodList, error) {
-	config, err := readConfig(context)
+	config, err := ReadConfig(context)
 
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := createClient(config)
+	client, err := CreateClient(config)
 
 	if err != nil {
 		return nil, err
 	}
 
-	pods, err := client.Core().Pods(environment).List(v1.ListOptions{})
+	return client.Core().Pods(environment).List(v1.ListOptions{})
+}
+
+func FindPodByContainer(context string, environment string, container string) (v1.Pod, error) {
+	pods, err := FetchPods(context, environment)
+
 	if err != nil {
-		return nil, err
+		return v1.Pod{}, err
 	}
 
-	return pods, nil
+	for _, pod := range pods.Items {
+		if strings.HasPrefix(pod.GetName(), container) {
+			return pod, nil
+		}
+	}
+
+	return v1.Pod{}, errors.New("Pods were found but not for the container specified")
+
 }
