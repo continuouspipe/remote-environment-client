37e4ccfb3d290c3c7e4b0c8713fbb0b91d66376e
Alessandro Zucca <alessandro.zucca@gmail.com>
2017-03-14 08:06:56 -0700

    Showing all public endpoints with correct protocol on watch, init and build (#92)
    * when building a remote environment in the cmd build or cmd init always git push
    * showing all public endpoints names and ip addresses after build and init
    * on watch and at the end of build and init we are displaying each public endpoints using the protocol://<ip-address> based on the port number when is either 80 or 433 or <ip-address>:<port> when is not.

--- cmd/build_test.go
+++ cmd/build_test.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/continuouspipe/remote-environment-client/config"
+	"github.com/continuouspipe/remote-environment-client/cpapi"
 	"github.com/continuouspipe/remote-environment-client/test/spies"
 	"testing"
 )
@@ -23,12 +25,43 @@ func TestRemoteBranchNotPresent(t *testing.T) {
 	spyWaitForEnvironmentReadyState.MockHandle(func() error {
 		return nil
 	})
+	spyApiProvider := spies.NewSpyApiProvider()
+	spyApiProvider.MockGetRemoteEnvironmentStatus(func(flowId string, environmentId string) (*cpapi.ApiRemoteEnvironmentStatus, error) {
+		r := &cpapi.ApiRemoteEnvironmentStatus{}
+		r.PublicEndpoints = []cpapi.ApiPublicEndpoint{
+			{
+				Address: "10.0.0.0",
+				Name:    "web",
+				Ports: []cpapi.ApiPublicEndpointPort{
+					{
+						Number:   80,
+						Protocol: "tcp",
+					},
+				},
+			},
+		}
+		return r, nil
+	})
+	spyConfig := spies.NewSpyConfig()
+	spyConfig.MockGetString(func(key string) (string, error) {
+		switch key {
+		case config.ApiKey:
+			return "some-api-key", nil
+		case config.RemoteEnvironmentId:
+			return "987654321", nil
+		case config.FlowId:
+			return "837d92hd-19su1d91", nil
+		}
+		return "", nil
+	})
 
 	//test subject called
 	buildHandle := BuildHandle{}
 	buildHandle.triggerBuild = spyTriggerBuild
 	buildHandle.waitForEnvironmentReady = spyWaitForEnvironmentReadyState
 	buildHandle.stdout = mockStdout
+	buildHandle.config = spyConfig
+	buildHandle.api = spyApiProvider
 	buildHandle.Handle()
 
 	//expectations
