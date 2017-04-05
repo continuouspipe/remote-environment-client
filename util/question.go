011b27ebe4ceb003670d49242469daf495015f08
Alessandro Zucca <alessandro.zucca@gmail.com>
2017-04-05 05:04:28 -0700

    Bugfix/cd 375 windows issue with crlf (#104)
    * swapping readString with readLine and adding documentation

--- util/question.go
+++ util/question.go
@@ -8,43 +8,27 @@ import (
 	"strings"
 )
 
+// QuestionPrompter prints a question in the stdout and handle the response by applying a default value or repeating the question
+// until a specific condition is validated
 type QuestionPrompter interface {
-	ReadString(string) string
 	ApplyDefault(string, string) string
 	RepeatIfEmpty(string) string
 	RepeatUntilValid(string, func(string) (bool, error)) string
 	RepeatPasswordIfEmpty(string) string
 	RepeatPasswordUntilValid(string, func(string) (bool, error)) string
-	ReadPassword(string) string
 }
 
+// QuestionPrompt implements QuestionPrompter and allows us to request the user a question and handles the answer in different ways
 type QuestionPrompt struct{}
 
+// NewQuestionPrompt constructor for QuestionPrompt
 func NewQuestionPrompt() *QuestionPrompt {
 	return &QuestionPrompt{}
 }
 
-func (qp QuestionPrompt) ReadString(question string) string {
-	fmt.Print(question, " ")
-	reader := bufio.NewReader(os.Stdin)
-	res, err := reader.ReadString('\n')
-	if err != nil {
-		res = ""
-	}
-	return strings.TrimRight(res, "\n")
-}
-
-func (qp QuestionPrompt) ReadPassword(q string) string {
-	fmt.Print(q, " ")
-	res, err := terminal.ReadPassword(0)
-	if err != nil {
-		res = []byte{}
-	}
-	return string(res)
-}
-
+// ApplyDefault if the answer is empty it returns a default value
 func (qp QuestionPrompt) ApplyDefault(question string, predef string) string {
-	res := qp.ReadString(question)
+	res := qp.readString(question)
 
 	if res == "" && predef != "" {
 		return predef
@@ -52,31 +36,34 @@ func (qp QuestionPrompt) ApplyDefault(question string, predef string) string {
 	return res
 }
 
+// RepeatIfEmpty requires the user to answer the question. It repeats the question until he answers.
 func (qp QuestionPrompt) RepeatIfEmpty(question string) string {
 	return qp.RepeatUntilValid(question, func(response string) (bool, error) {
 		valid := len(response) > 0
 		if !valid {
-			return false, fmt.Errorf("Please insert a value.")
+			return false, fmt.Errorf("please insert a value")
 		}
 		return valid, nil
 	})
 }
 
+// RepeatPasswordIfEmpty requires the user to answer the question by inserting an answer that wont be shown when is typed.
+// It repeats the question until he answers.
 func (qp QuestionPrompt) RepeatPasswordIfEmpty(question string) string {
 	return qp.RepeatPasswordUntilValid(question, func(response string) (bool, error) {
 		valid := len(response) > 0
 		if !valid {
-			return false, fmt.Errorf("Please insert a value.")
+			return false, fmt.Errorf("please insert a value")
 		}
 		return valid, nil
 	})
 }
 
-//ask the same question to the user until the isValid() callback returns true
+// RepeatUntilValid ask the same question to the user until the isValid() callback returns true
 func (qp QuestionPrompt) RepeatUntilValid(question string, isValid func(string) (bool, error)) string {
 	var res string
 	for {
-		res = qp.ReadString(question)
+		res = qp.readString(question)
 		isValid, err := isValid(res)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -88,11 +75,11 @@ func (qp QuestionPrompt) RepeatUntilValid(question string, isValid func(string) 
 	return res
 }
 
-//ask the same question to the user until the isValid() callback returns true
+// RepeatPasswordUntilValid ask the same question to the user until the isValid() callback returns true
 func (qp QuestionPrompt) RepeatPasswordUntilValid(question string, isValid func(string) (bool, error)) string {
 	var res string
 	for {
-		res = qp.ReadPassword(question)
+		res = qp.readPassword(question)
 		isValid, err := isValid(res)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -102,4 +89,24 @@ func (qp QuestionPrompt) RepeatPasswordUntilValid(question string, isValid func(
 		}
 	}
 	return res
+}
+
+func (qp QuestionPrompt) readString(question string) string {
+	fmt.Print(question, " ")
+	reader := bufio.NewReader(os.Stdin)
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		line = []byte{}
+	}
+
+	return strings.TrimRight(string(line), " \r\n")
+}
+
+func (qp QuestionPrompt) readPassword(q string) string {
+	fmt.Print(q, " ")
+	res, err := terminal.ReadPassword(0)
+	if err != nil {
+		res = []byte{}
+	}
+	return string(res)
 }
